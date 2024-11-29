841fb475c1114ec851cb16dcc086ef76f7379dd4
Marcos Venicio <marcos.venicio1230@hotmail.com>
2024-11-29 12:12:14 -0300

    Iniciando configuração do Elastic

--- Program.cs
+++ Program.cs
@@ -4,39 +4,55 @@ using ApiUser.Repository;
 using ApiUser.Services;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-var connectionString = builder.Configuration.GetConnectionString("UserDatabase");
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<UserContext>(options =>
+try
 {
-    options.UseSqlServer(connectionString).LogTo(Console.WriteLine, LogLevel.Information);
-});
+    var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IUserService, UserServices>();
-builder.Services.AddScoped<UserContext>();
+    // Elastic
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+    // Add services to the container.
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+    var connectionString = builder.Configuration.GetConnectionString("UserDatabase");
+
+    builder.Services.AddControllers();
+    builder.Services.AddDbContext<UserContext>(options =>
+    {
+        options.UseSqlServer(connectionString).LogTo(Console.WriteLine, LogLevel.Information);
+    });
+
+    builder.Services.AddScoped<IUserRepository, UserRepository>();
+    builder.Services.AddScoped<IUserService, UserServices>();
+    builder.Services.AddScoped<UserContext>();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+     app.Run();
+}
+catch (Exception ex)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //Log.Fatal(ex, "Host terminated unexpectedly");
+}
+finally
+{
+    //Log.Information("Server Shutting down...");
+    //Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
